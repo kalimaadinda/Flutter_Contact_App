557a3b374612ecbc63afdd0aabd2e969e9fe6b72
kalimaadinda <kalimaadinda5@gmail>
2021-12-15 16:56:52 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+import 'home_view.dart';
+ 
+void main() {
+  runApp(
+    DevicePreview(
+      builder: (context) => const MyApp()));
+}
+ 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      useInheritedMediaQuery: true,
+      title: 'Contact App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  HomeView(),
+    );
+  }
+}
